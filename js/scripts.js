41b1e62476f5b17205fe4db5a4b61f6183708dc6
Caitlin Ashtari <caitlinashtari@gmail.com>
2016-10-03 19:16:29 -0700

    refactor js to create a single evaluation to check if year is divisible by 4 or 100.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
 /*Back-end -----------------------*/
 var leapYear = function(year){
-  if (year % 100 === 0){
+  if ((year % 100 !== 0) && (year % 4 === 0)){
     return false;
-  } else if (year % 4 === 0){
-    return true;
   } else {
     return false;
   }
